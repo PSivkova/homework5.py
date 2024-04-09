2b0347ef3219cf97b04e43948b5671e1000f9cde
PSivkova <165670995+PSivkova@users.noreply.github.com>
2024-04-10 00:49:00 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,15 @@
+my_list = ['apple', 'banana', 'orange', 'kiwi', 'Mango', 'Papaya']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'Avocado'
+print(my_list)
+
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин'}
+print(my_dict)
+print(my_dict['apple'])
+my_dict['apple'] = 0
+print(my_dict)
+my_dict['Coconut'] = 'кокос'
+print(my_dict)
